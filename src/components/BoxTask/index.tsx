77bf42bae1214b3181c40ce32ad77dd828ebeb6c
olivpuddin <bububeatriz@icloud.com>
2022-02-21 17:15:01 -0300

    feat: section for tasks area and required inputs

--- src/components/BoxTask/index.tsx
+++ src/components/BoxTask/index.tsx
@@ -1,10 +1,26 @@
 import React from "react";
 import './style.css';
 
-export const Box = () => {
+interface Props {
+    title: string,
+    place: string,
+    notes: string
+}
+
+export const Box = ({ title, place, notes }: Props) => {
     return (
-        <ul className="tasks">
-            <li>this morning's meeting, we have to improve the quality of office facilities and another...</li>
-        </ul>
+        <div className="all">
+            
+            <div className="box">
+                <div className="content">                 
+                    <h1>{title}</h1>
+                    <p>{place}</p>
+                </div>
+
+                <div className="notes">
+                    <p>{notes}</p>
+                </div>
+            </div>
+        </div>
     )
 }
--- src/screens/tasksScreen/index.tsx
+++ src/screens/tasksScreen/index.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
 import { Header } from "../../components/Header";
 import { NewTask } from "../../components/NewTask";
 import './style.css';
@@ -7,7 +8,36 @@ import pic from '../../assets/foto-1.png';
 import picture from '../../assets/foto-3.png';
 
 
+interface Itasks {
+    id: string, 
+    title: string, 
+    startFrom: string, 
+    finish: string,
+    place: string, 
+    notes: string
+}
+
 export const SecondScreen = () => {
+    const [tasks, setTasks] = useState<Itasks[]>([]);
+    const { register, handleSubmit, reset, formState: { errors } } = useForm<Itasks>();
+
+    const onSubmit: SubmitHandler<Itasks> = (data) => {
+       setTasks((old) => [
+           ...old,
+           {
+                id: data.id,
+                title: data.title, 
+                startFrom: data.id, 
+                finish: data.id,
+                place: data.place, 
+                notes: data.notes
+           }
+       ])
+       reset()
+    };
+
+    
+
     return (
         <div className="wrapper">
             <Header />
@@ -15,24 +45,34 @@ export const SecondScreen = () => {
             <div className="body">
 
                 <div className="body-left">
-                    <p className="task-text">New Task</p>
-                    <input type='text' placeholder='Title' className="input" />
-                    <NewTask title='Start from' info='Mon, 20 Sep 2021 10:00 AM' />
-                    <NewTask title='Finish' info='Mon, 20 Sep 2021 11:00 AM' />
-                    <NewTask title='Repeat' info='One time' />
-                    <NewTask title='Reminder' info='Before 5 minutes' />
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <p className="task-text">New Task</p>
+                        <input id="title" type='text' placeholder='Title' className="input" {...register("title", { required: true })}/>
+                        {errors.title && errors.title.type === "required" && <span className="span">This is required</span>}
 
-                    <input className="input" placeholder="Place" />
-                    <input className="input" placeholder="Notes" />
-                    <button type="submit" className="submit">Cadastrar</button>
+                        <NewTask title='Start from' info='Mon, 20 Sep 2021 10:00 AM' />
+                        <NewTask title='Finish' info='Mon, 20 Sep 2021 11:00 AM' />
+                   
+                        <input id="place" className="input" placeholder="Place" {...register("place", { required: true })}  />
+                        {errors.place && errors.place.type === "required" && <span className="span">This is required</span>}
+
+                        <input id="notes" className="input" placeholder="Notes" {...register("notes", { required: true })}/>
+                        {errors.notes && errors.notes.type === "required" && <span className="span">This is required</span>}
+
+                        <button type="submit" className="submit">Cadastrar</button>
+                    </form>
                 </div>
 
-                <div className="body-right">
-                    <p>To DO</p>
-                    <Box />
-                    <Box />
-                    <Box />
-                </div>
+                { tasks.length > 0 && <div className="body-right">
+                    <section className="section">
+                        <p>To DO</p>
+                        {
+                            tasks.map((item) => (
+                                <Box title={item.title} place={item.place} notes={item.notes}/>
+                            ))
+                        }
+                    </section>
+                </div>}
             </div>
 
             <div className="footer">
